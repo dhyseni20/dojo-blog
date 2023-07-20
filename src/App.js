942470c8da20f843b0618160488c9c63719ddbd7
Drilon Hyseni <emilianhyseni@gmail.com>
2023-07-20 18:53:19 +0200

    Lesson 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 
 function App() {
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  const link = "https://www.google.com";
   return (
     <div className="App">
       <div className="content">
         <h1> App Component</h1>
+        <h1> {title}</h1>
+        <p>Liked {likes} times </p>
+        <p>{Math.random() * 100}</p>
+        <a href={link}>Google Sites</a>
       </div>
     </div>
   );
