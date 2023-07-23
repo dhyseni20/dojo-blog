92e881750d0ac492d8135a9326cb4de053a19cb5
Drilon Hyseni <emilianhyseni@gmail.com>
2023-07-23 13:03:58 +0200

    Lesson 6 - CSS

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import "./App.css";
 import Navbar from "./Navbar";
 import Home from "./Home";
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  const link = "https://www.google.com";
+  // const title = "Welcome to the new blog";
+  // const likes = 50;
+  // const link = "https://www.google.com";
   return (
     <div className="App">
       <Navbar />
       <div className="content">
         <Home />
-        <h1> App Component</h1>
+        {/* <h1> App Component</h1>
         <h3> {title}</h3>
         <p>Liked {likes} times </p>
-        <p>{Math.random() * 100}</p>
-        <a href={link}>Google Sites</a>
+        {/* <p>{Math.random() * 100}</p> 
+        <a href={link}>Google Sites</a> */}
       </div>
     </div>
   );
