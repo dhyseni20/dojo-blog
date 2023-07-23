e0bbe4219f900fd9297587d4292b272663590387
Drilon Hyseni <emilianhyseni@gmail.com>
2023-07-23 16:07:43 +0200

    Lesson7 - Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
 const Home = () => {
+  const handleClick = function (e) {
+    console.log("Heloo Drilon", e);
+  };
+  const handleClickAgain = (name) => {
+    console.log("hello " + name);
+  };
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button
+        onClick={() => {
+          handleClickAgain("drilon");
+        }}
+      >
+        Click me again
+      </button>
     </div>
   );
 };
