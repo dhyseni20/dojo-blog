7b9eb5f666a8ebecd7fc82ae57716e12e8e6163a
Drilon Hyseni <emilianhyseni@gmail.com>
2023-07-23 12:53:13 +0200

    Lesson 5 - Adding Components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
   const title = "Welcome to the new blog";
@@ -6,9 +8,11 @@ function App() {
   const link = "https://www.google.com";
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
+        <Home />
         <h1> App Component</h1>
-        <h1> {title}</h1>
+        <h3> {title}</h3>
         <p>Liked {likes} times </p>
         <p>{Math.random() * 100}</p>
         <a href={link}>Google Sites</a>
