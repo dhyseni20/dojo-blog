f7a2ac8c626fcd2e8355bbb65086df6a362baa48
Drilon Hyseni <emilianhyseni@gmail.com>
2023-07-23 16:55:09 +0200

    Lesson 10 - Lists

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,24 @@
 import { useState } from "react";
 
 const Home = () => {
-  //  +++ let name = "Drilon";
-  const [name, setName] = useState("drilon");
-  const [age, setAge] = useState(21);
-  const handleClick = function (e) {
-    setName("luigi");
-    setAge(34);
-  };
-
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>
-        {name} is {age} years old.
-      </p>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
