dbcc9875ae53e703e53dfcc27d2809e5c4e5e5d1
Drilon Hyseni <emilianhyseni@gmail.com>
2023-07-24 17:39:44 +0200

    Lesson 17 - Fetching Data with useEffect

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({ blogs, title, handleDelete }) => {
+const BlogList = ({ blogs, title }) => {
   // destruct {blogs, title}
   //   const blogs = props.blogs;
   //   const title = props.title;
@@ -10,16 +10,6 @@ const BlogList = ({ blogs, title, handleDelete }) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
-          <button
-            onClick={() => handleDelete(blog.id)}
-            style={{
-              color: "white",
-              backgroundColor: "#f1356d",
-              borderRadius: "8px",
-            }}
-          >
-            delete blog
-          </button>
         </div>
       ))}
     </div>
