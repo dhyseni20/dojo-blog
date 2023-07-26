de2be00f24d4da3d5461f278be696f4b466cb9bc
Drilon Hyseni <emilianhyseni@gmail.com>
2023-07-26 15:09:51 +0200

    Lesson 21- React Route

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  // const title = "Welcome to the new blog";
-  // const likes = 50;
-  // const link = "https://www.google.com";
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-        {/* <h1> App Component</h1>
-        <h3> {title}</h3>
-        <p>Liked {likes} times </p>
-        {/* <p>{Math.random() * 100}</p> 
-        <a href={link}>Google Sites</a> */}
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
