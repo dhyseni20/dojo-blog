8cc7282a4582d86b308fcecda118e465a08062a1
Drilon Hyseni <emilianhyseni@gmail.com>
2023-07-26 15:22:52 +0200

    Lesson 22- Exact Route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Create from "./Create";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
         <Navbar />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home />}></Route>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route path="/create" element={<Create />}></Route>
           </Routes>
         </div>
       </div>
